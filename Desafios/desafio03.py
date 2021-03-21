0e6a407538d6106e3f8084319f37d1986dd2eb45
JeffynhoG3 <jeffynho.7@gmail.com>
2021-03-21 16:21:23 -0300

    Formatação do texto
    Formatação e estética

---
+++ Desafios/desafio03.py
@@ -0,0 +1,9 @@
+print('')
+print('')
+print('===== DESAFIO 3 =====')
+print('')
+n1 = int(input('Primerio número: '))
+n2 = int(input('Segundo número: '))
+soma = (n1+n2)
+print('A soma entre {} e {} é igual a {}.'.format(n1, n2, soma))
+
--- Desafios/desafio3.py
+++
@@ -1,5 +0,0 @@
-print ('===== DESAFIO 3 =====')
-n1 = int (input('Primeiro numero: '))
-n2 = int (input('Segundo numero: '))
-s = n1+n2
-print ('A some vale {}'.format(s))
