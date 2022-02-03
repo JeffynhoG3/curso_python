9124fec2091cdcffb617d619d9678c8621338134
JeffynhoG3 <jeffynho.7@gmail.com>
2022-02-02 21:38:59 -0300

    Create ex002.py

---
+++ PythonExercicios/ex002.py
@@ -0,0 +1,7 @@
+#Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+
+print('')
+print('===== Exercício #002 =====')
+print('')
+nome = input('Qual é o seu nome: ')
+print('E um prazer em te conhecer, {}!'.format(nome))
