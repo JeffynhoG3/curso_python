a91b446c4e0a4460f8ccfabc87aa12c70bb5d0ac
JeffynhoG3 <jeffynho.7@gmail.com>
2022-02-05 20:35:54 -0300

    Update desafio04.py

--- desafios/desafio04.py
+++ desafios/desafio04.py
@@ -1,3 +1,7 @@
+# Faça um programa que leia algo pelo teclado e mostre
+# na tela o seu tipo primitivo e todas as informações
+# possíveis sobre ele.
+
 print('')
 print('')
 print('===== DESAFIO 4 =====')
@@ -9,7 +13,7 @@ print('E alfanumérico:', n1.isalnum())
 print('Ele e um número:', n1.isnumeric())
 print('Ele e alfabético:', n1.isalpha())
 print('Ele está em letras maiúsculas:', n1.isupper())
-print('Ele está em letras minúsculas:',n1.islower())
+print('Ele está em letras minúsculas:', n1.islower())
 print('Ele tem espaços:', n1.isspace())
 print('Ele pode ser impresso:', n1.isprintable())
 print('Ele está captalizada: ', n1.istitle())
