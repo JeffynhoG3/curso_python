08affe745bb32774390e2d8ffb4392ca977da300
JeffynhoG3 <jeffynho.7@gmail.com>
2022-02-05 20:46:54 -0300

    Create ex004.py

---
+++ PythonExercicios/ex004.py
@@ -0,0 +1,19 @@
+# Faça um programa que leia algo pelo teclado e mostre
+# na tela o seu tipo primitivo e todas as informações
+# possíveis sobre ele.
+
+print('')
+print('===== Exercício #004 =====')
+print('')
+
+n1 = input('Digite algo: ')
+
+print('Seu tipo primitivo é:', type(n1))
+print('E alfanumérico:', n1.isalnum())
+print('Ele e um número:', n1.isnumeric())
+print('Ele e alfabético:', n1.isalpha())
+print('Ele está em letras maiúsculas:', n1.isupper())
+print('Ele está em letras minúsculas:', n1.islower())
+print('Só tem espaços:', n1.isspace())
+print('Ele pode ser impresso:', n1.isprintable())
+print('Ele está captalizada: ', n1.istitle())
