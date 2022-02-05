54dfe8837f274a55ea5a365214043a543fb81e5e
JeffynhoG3 <jeffynho.7@gmail.com>
2022-02-05 20:35:50 -0300

    Create ex003.py

---
+++ PythonExercicios/ex003.py
@@ -0,0 +1,10 @@
+# Crie um programa que leia dois números e mostre a soma entre eles.
+
+print('')
+print('===== Exercício #002 =====')
+print('')
+
+n1 = int(input('Primerio número: '))
+n2 = int(input('Segundo número: '))
+soma = (n1+n2)
+print('A soma entre {} e {} é igual a {}.'.format(n1, n2, soma))
