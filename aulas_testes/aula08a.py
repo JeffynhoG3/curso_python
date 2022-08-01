7cfde59716f8a8730895e528fb2f8f7eedfcf751
JeffynhoG3 <jeffynho.7@gmail.com>
2022-07-31 23:37:31 -0300

    aula 8

---
+++ aulas_testes/aula08a.py
@@ -0,0 +1,12 @@
+# import math
+# from math import sqrt, floor
+from math import sqrt
+
+num = int(input('Digite um número: '))
+
+# raiz = math.sqrt(num)
+raiz = sqrt(num)
+
+# print('A raiz de {} é igual a {}'.format(num, raiz))
+# print('A raiz de {} é igual a {}'.format(num, math.trunc(raiz)))
+print('A raiz de {} é igual a {:.2f}'.format(num, (raiz)))
